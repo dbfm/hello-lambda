fd2527b6c390366b2b9da0a23f3b15f4d6249124
Chris Thomas <chris@dbfm.net>
2022-07-22 15:59:54 +0100

    nothing change to trigger workflow

--- deployment/main.tf
+++ deployment/main.tf
@@ -5,8 +5,8 @@ module "hello_lambda" {
   function_name                     = "hello-${terraform.workspace}"
   handler                           = "index.handler"
   runtime                           = "nodejs14.x"
-  create_lambda_function_url        = true
   use_existing_cloudwatch_log_group = false
+  create_lambda_function_url        = true
   cloudwatch_logs_retention_in_days = 1
   source_path = [
     "${path.module}/../src/index.js"
